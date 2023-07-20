454d535dc242218c5596a0956d6f0a176512ee00
reolinodev <reolino.dev@gmail.com>
2023-07-20 10:54:00 +0900

    로그 두번 나오는 문제 처리

--- src/main/java/com/back/support/P6spyPrettySqlFormatter.java
+++ src/main/java/com/back/support/P6spyPrettySqlFormatter.java
@@ -11,13 +11,14 @@ public class P6spyPrettySqlFormatter implements MessageFormattingStrategy {
 
     @Override
     public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
+        if(!url.contains("p6spy")){
+            return "";
+        }
+
         sql = formatSql(category, sql);
         Date currentDate = new Date();
 
         SimpleDateFormat format1 = new SimpleDateFormat("yy.MM.dd HH:mm:ss");
-
-        System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<2");
-
 
         return format1.format(currentDate) + " | "+ "OperationTime : "+ elapsed + "ms" + sql;
     }
@@ -25,9 +26,6 @@ public class P6spyPrettySqlFormatter implements MessageFormattingStrategy {
     private String formatSql(String category,String sql) {
         if(sql ==null || sql.trim().equals("")) return sql;
 
-        System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<1");
-
-        // Only format Statement, distinguish DDL And DML
         if (Category.STATEMENT.getName().equals(category)) {
             String tmpsql = sql.trim().toLowerCase(Locale.ROOT);
             if(tmpsql.startsWith("create") || tmpsql.startsWith("alter") || tmpsql.startsWith("comment")) {
